744c185594234975d0ca3414f68b27b6070f898d
Fred <huertaserick@yahoo.ie>
2025-03-14 18:44:29 +0000

    jest jest jest testing omg confusing

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  transformIgnorePatterns: ["/node_modules/(?!ol.*)/"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest", // Ensure Babel transforms JS files
-  },
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
 };
+
+module.exports = createJestConfig(customJestConfig);
